088d22d6171b29e76293a3f4f1972e410486750c
frandrew90 <frandrewjs@gmail.com>
2021-09-09 17:57:49 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import './sass/styles.css';
+import fetchPictures from './js/apiService';
+import refs from './js/refs';
+import makeMarkup from './templates/card.hbs';
+
+console.log(fetchPictures('red roses', 1));
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const input = e.currentTarget.elements.query.value;
+  return input;
+}
+
+// console.log(input);
---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+  form: document.querySelector('#search-form'),
+  inputRef: document.querySelector('.js_input'),
+  searchBtnRef: document.querySelector('.js_search_btn'),
+  loadBtnRef: document.querySelector('.js_load_btn'),
+  listRef: document.querySelector('.'),
+};
+
+export default refs;
